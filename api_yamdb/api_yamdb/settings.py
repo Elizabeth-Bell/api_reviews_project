d2ef9b263437785f22effbe85f000e776bd901b8
Елизавета Беляева <mollymyn@yandex.ru>
2023-06-27 16:23:45 +0300

    первый драфт title

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'reviews.apps.ReviewsConfig',
+    'api.apps.ApiConfig',
+
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Title(models.Model):
+    name = models.TextField(max_length=256, verbose_name='Название')
+    year = models.IntegerField(verbose_name='Год выпуска')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    #genre = models.ManyToManyField(Genre, through = 'TitleGenres',
+                              #verbose_name='Slug жанра')
+    #category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 #related_name='titles',
+                                 #verbose_name='Slug категории'
+                                 #)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name[:20]
+
+
+class TitleGenres(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
