0006bfd918b5205362572534cf9a166cfa51b885
Елизавета Беляева <mollymyn@yandex.ru>
2023-06-30 16:04:43 +0300

    Создан кастомный вьюсет с ограничением типа запросов под жанры, добавлен лукап филд

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,16 @@
 from reviews.models import Title, Genre
-from rest_framework import viewsets, filters
+
+from rest_framework import viewsets, filters, mixins
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import TitleSerializer, GenreSerializer
+
+
+class ListCreateDestroyViewSet(mixins.ListModelMixin,
+                               mixins.CreateModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
+    lookup_field = 'slug'
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -12,7 +20,7 @@ class TitleViewSet(viewsets.ModelViewSet):
     filterset_fields = ('name', 'year', 'genres__slug') #'category',
 
 
-class GenreViewSet(viewsets.ModelViewSet):
+class GenreViewSet(ListCreateDestroyViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter,)
