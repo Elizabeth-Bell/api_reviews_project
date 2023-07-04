33dd04071d1d68b81df528814131d5d4cdd3183e
Евгений Киричек <geka19971997@yandex.ru>
2023-07-04 21:21:12 +0300

    Migrations

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -71,3 +71,99 @@ class TokenSerializer(serializers.Serializer):
     class Meta:
         fields = ("username", "confirmation_code")
         model = CustomUser
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        exclude = ("id",)
+        model = Category
+        lookup_field = "slug"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        exclude = ("id",)
+        lookup_field = "slug"
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(many=True, read_only=True)
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        fields = "__all__"
+        model = Title
+
+
+class TitlePostSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field="slug", queryset=Genre.objects.all(), many=True
+    )
+    category = serializers.SlugRelatedField(
+        slug_field="slug", queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = "__all__"
+
+    def validate_year(self, value):
+        current_year = timezone.now().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                "Год не может быть больше текущего года.")
+        return value
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для отзывов."""
+    author = serializers.SlugRelatedField(
+        slug_field="username",
+        read_only=True,
+    )
+
+    class Meta:
+        model = Review
+        fields = (
+            "id",
+            "text",
+            "author",
+            "score",
+            "pub_date",
+        )
+
+    def validate(self, data):
+        title_id = self.context["request"].parser_context["kwargs"]["title_id"]
+        user = self.context["request"].user
+        if (
+            self.context["request"].method == "POST"
+            and Review.objects.filter(author=user, title=title_id).exists()
+        ):
+            raise serializers.ValidationError(
+                "Вы уже оставляли отзыв на это произведение."
+            )
+        return data
+
+    def validate_score(self, value):
+        if value < 1 or value > 10:
+            raise serializers.ValidationError("Оценка должна быть от 1 до 10.")
+        return value
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для комментариев"""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = (
+            'id',
+            'text',
+            'author',
+            'pub_date',
+        )
+        model = Comment
