bd622bbd79926a7563268a17db1ba0e66bbd91fa
Евгений Киричек <geka19971997@yandex.ru>
2023-07-05 15:05:21 +0300

    Пермишен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,36 +1,40 @@
 from rest_framework import permissions
 
-from users.models import CustomUser
 
-
-class IsAdmin(permissions.BasePermission):
-    allowed = CustomUser.ADMINISTRATOR
+class IsAdminModeratorAuthor(permissions.BasePermission):
+    """Доступ на чтение, только пользователю."""
 
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated and request.user.role in self.allowed
-        ) or request.user.is_superuser
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.role == CustomUser.ADMINISTRATOR
-        ) or request.method in permissions.SAFE_METHODS
-
-
-class IsUserAdminModeratorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            or request.method in permissions.SAFE_METHODS
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.role != CustomUser.USER
-            or (request.user.role == CustomUser.USER
-                and obj.author == request.user)
+            or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
+        )
+
+
+class IsAdmin(permissions.BasePermission):
+    """Полный доступ Админа."""
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin
+        )
+
+
+class IsAdminOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    """Внесение изменений только с правом администратора"""
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_admin
         )
