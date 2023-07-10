bd584229555e368713bb0c8846a29ec435ec8c4a
Elizabeth-Bell <125978340+Elizabeth-Bell@users.noreply.github.com>
2023-07-10 22:22:42 +0300

    Develop second review (#24)
    * Исправлены замечания по итогам второго ревью
    
    * Исправлена валидация года
    
    * Исправлены все замечания кроме me-admin

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
@@ -6,4 +7,6 @@ def validate_year(value):
     """Валидатор года, не больше текущего"""
     current_year = timezone.now().year
     if value >= current_year:
-        raise ValidationError("Год не может быть больше текущего")
+        raise ValidationError('Год не может быть больше текущего')
+    elif value <= settings.MAX_SCORE_VALUE:
+        raise ValidationError('Год не может быть отрицательным')
